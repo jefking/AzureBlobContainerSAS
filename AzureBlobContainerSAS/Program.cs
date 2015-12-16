d803a44bfd1995407c2c0bd8368bb1d9e6eb59b8
Jef King <jefkin@microsoft.com>
2015-12-16 12:04:16 -0800

    booyucka

---
+++ AzureBlobContainerSAS/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace AzureBlobContainerSAS
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Auth;
+    using Microsoft.WindowsAzure.Storage.Blob;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var creds = new StorageCredentials("", "");
+            var storageAccount = new CloudStorageAccount(creds, true);
+
+            //Create the blob client object.
+            var blobClient = storageAccount.CreateCloudBlobClient();
+
+            //Get a reference to a container to use for the sample code, and create it if it does not exist.
+            var container = blobClient.GetContainerReference("sascontainer");
+            container.CreateIfNotExists();
+
+            var sasuri = GetContainerSasUri(container);
+
+            Console.WriteLine(sasuri);
+
+            //Require user input before closing the console window.
+            Console.ReadLine();
+        }
+
+        static string GetContainerSasUri(CloudBlobContainer container)
+        {
+            //Set the expiry time and permissions for the container.
+            //In this case no start time is specified, so the shared access signature becomes valid immediately.
+            var sasConstraints = new SharedAccessBlobPolicy();
+            sasConstraints.SharedAccessExpiryTime = DateTime.UtcNow.AddHours(24);
+            sasConstraints.Permissions = SharedAccessBlobPermissions.Write | SharedAccessBlobPermissions.List;
+
+            //Generate the shared access signature on the container, setting the constraints directly on the signature.
+            var sasContainerToken = container.GetSharedAccessSignature(sasConstraints);
+
+            //Return the URI string for the container, including the SAS token.
+            return container.Uri + sasContainerToken;
+        }
+    }
+}
